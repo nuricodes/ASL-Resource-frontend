c45ca3b7c8baf193ed3010e6c77d5299e9dd641a
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2020-10-25 19:13:29 -0500

    added components and a starter boilerplate

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Form = (props) => {
+  //STATE FOR THE FORM
+  const [formData, setFormData] = React.useState(props.word);
+
+  //FUNCTIONS
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent Form from Refreshing
+    props.handleSubmit(formData); // Submit to Parents desired function
+    props.history.push("/"); //Push back to display page
+  };
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="word"
+        value={formData.word}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="img"
+        value={formData.img}
+        onChange={handleChange}
+      />
+      <input type="submit" value={props.label} />
+    </form>
+  );
+};
+
+export default Form;
