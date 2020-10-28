e1f96693cbd2a8d5c6f1f6d5813fe539c6fb2ccd
ayninur <nuralaynabdulqadir@gmail.com>
2020-10-28 19:33:49 -0400

    signup page is almost complete

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,17 +3,23 @@ import '../App.css';
 import { Button } from './Button';
 import './Hero.css'
 
+
+
 function Hero() {
     return (
-        <div className="hero-container">
-            {/* <video src="https://vimeo.com/user125927561/review/472735427/ab42bf207f" autoPlay loop muted /> */}
-            <h1>Your Language Resource</h1>
-            <p>Find the quick links you need to get by</p>
-            <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>SIGN UP TODAY</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Filtered Search</Button>
+        <>
+
+            <div className="hero-container">
+
+                {/* <video src="https://vimeo.com/user125927561/review/472735427/ab42bf207f" autoPlay loop muted /> */}
+                <h1>Your Language Resource</h1>
+                <p>Find the quick links you need to get by</p>
+                <div className="hero-btns">
+                    <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>SIGN UP TODAY</Button>
+                    <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Filtered Search</Button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/components/pages/Join/Login.js
+++ src/components/pages/Join/Login.js
@@ -3,14 +3,14 @@ import useForm from './useForm'
 import validate from './validateInfo'
 import './Form.css'
 
-const FormSignup = ({ submitForm }) => {
+const Login = ({ submitForm }) => {
     const { handleChange, values, handleSubmit, errors } = useForm(submitForm, validate)
 
 
     return (
         <div className="form-content-right">
             <form className="form" onSubmit={handleSubmit}>
-                <h1>Get started with us today! Create your account by filling out the information below.</h1>
+                <img src="https://www.iconspng.com/images/-abstract-user-icon-1/-abstract-user-icon-1.jpg" height="150px" width="150px" />
                 <div className="form-inputs">
                     <label htmlFor="username" className="form-label">
                         Username:
@@ -41,29 +41,16 @@ const FormSignup = ({ submitForm }) => {
                         onChange={handleChange} />
                     {errors.password && <p>{errors.password}</p>}
                 </div>
-                <div className="form-inputs">
-                    <label htmlFor="password2" className="form-label">
-                        Confirm Password:
-                    </label>
-                    <input
-                        id="password2"
-                        type="password"
-                        name='password2'
-                        className='form-input'
-                        placeholder="Enter your password"
-                        value={values.password2}
-                        onChange={handleChange} />
-                    {errors.password2 && <p>{errors.password2}</p>}
-                </div>
                 <button
                     className="form-input-btn"
                     type="submit">Submit</button>
                 <span className="form-input-login">
-                    Already have an account? Login <a
+                    Forgot your password? Click <a
                         href="#">here</a></span>
             </form>
         </div>
     )
 }
 
-export default FormSignup
+export default Login
+
