b723fa43799e14835328b9486f67607123536fd8
Bianca <biancalyngonzalez@gmail.com>
2020-10-29 10:54:37 -0700

    AUthGlobal.js fixed export default tag

--- src/AuthGlobal.js
+++ src/AuthGlobal.js
@@ -5,24 +5,25 @@ import {Route, Link, Switch} from 'react-router-dom'
 
 export const GlobalCtx = React.createContext(null)
 
-
-const Global = () => {
+const Global = () => { 
     const [gState, gStateSet] = React.useState({url: "http://localhost:3000"})
 
     return(
         <GlobalCtx.Provider value={(gState, setGState)}>
-            <div>
-            <Header />
+        <div>
+        <Header />
             <main>
                 <switch>
                     <Route exact path="/" render={(rp => <h1>Home</h1>)}/>
                     <Route path="/signup" render={(rp => <h1>Signup</h1>)}/>
                     <Route path="/login" render={(rp => <h1>Login</h1>)}/>
                     <Route path="/dashboard" render={(rp => <h1>Dashboard</h1>)}/>
-
-            </div>
+                </switch>
+            </main>
+        </div>
         </GlobalCtx.Provider>
-    )
-}
+        )
+    }
 
 export default Global
+
