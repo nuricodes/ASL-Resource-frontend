b70d18a8a6fcfd95b3b2f9415643bf6655fbe519
Bianca <biancalyngonzalez@gmail.com>
2020-10-29 10:50:07 -0700

    AuthGlobal.js edited

--- src/AuthGlobal.js
+++ src/AuthGlobal.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './App.css'
+import Header from './Header.js'
+import {Route, Link, Switch} from 'react-router-dom'
 
 export const GlobalCtx = React.createContext(null)
 
-const [gState, gStateSet] = React.useState({ursl: "http://localhost:3000"})
 
 const Global = () => {
+    const [gState, gStateSet] = React.useState({url: "http://localhost:3000"})
+
     return(
         <GlobalCtx.Provider value={(gState, setGState)}>
             <div>
+            <Header />
+            <main>
+                <switch>
+                    <Route exact path="/" render={(rp => <h1>Home</h1>)}/>
+                    <Route path="/signup" render={(rp => <h1>Signup</h1>)}/>
+                    <Route path="/login" render={(rp => <h1>Login</h1>)}/>
+                    <Route path="/dashboard" render={(rp => <h1>Dashboard</h1>)}/>
 
             </div>
         </GlobalCtx.Provider>
