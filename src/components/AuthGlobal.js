8ccf15c9da404c839f2f3a02c4e0f85f01534c8a
Bianca <biancalyngonzalez@gmail.com>
2020-10-29 13:58:42 -0700

    fixed index.html to include images

--- src/components/AuthGlobal.js
+++ src/components/AuthGlobal.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
 import Header from './Header'
-import {Route, link, Switch} from 'react-router-dom'
+import {Route, Link, Switch} from 'react-router-dom'
 
 export const GlobalCtx = React.createContext(null)
 
 const Global = () => { 
-    const [gState, gStateSet] = React.useState({url: "http://localhost:3000"})
+    const [gState, gStateSet] = React.useState({url: "http://localhost:4000"})
     return(
         <GlobalCtx.Provider value={(gState, gStateSet)}>
         <div>
