586821a7e0bcdfa9c250f3933d34e1b3a270a834
Bianca <biancalyngonzalez@gmail.com>
2020-10-29 15:01:53 -0700

    added header.js file

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Header = (props) => {
+    return (
+    <nav>
+        <Link to='/signup'>
+        </Link>
+
+        <Link to='/login'>
+        </Link>
+    </nav>
+    )
+}
+
+export default Header
