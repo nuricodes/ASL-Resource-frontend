b54b0e3defc49300298f6de17f24c98fc02cdf8a
Bianca <biancalyngonzalez@gmail.com>
2020-10-29 10:36:54 -0700

    created Header.js

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+    return <nav>
+        <Link to='/signup'>
+        </Link>
+        <Link to='/login'>
+        </Link>
+    </nav>
+}
+
+default export Header
